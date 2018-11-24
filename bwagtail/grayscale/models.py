4f242b94a6c89157ca39ddc08ae0411e6f0f90d4
Marc <mjlabe@gmail.com>
2018-11-24 14:35:52 -0500

    started Freeform page, but getting error rendering

--- bwagtail/grayscale/models.py
+++ bwagtail/grayscale/models.py
@@ -4,10 +4,28 @@ from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
+from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+from wagtail.core import blocks
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.images.blocks import ImageChooserBlock
+
 
 class GrayscalePage(Page):
     body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
+    ]
+
+
+class GrayscaleMastheadPage(Page):
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ])
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body'),
     ]
