c3fb1c33cd5916486dd1d6bef3860c8a523bbfe9
Marc <mjlabe@gmail.com>
2018-11-24 19:32:56 -0500

    added masthead block to grayscale app

--- bwagtail/grayscale/models.py
+++ bwagtail/grayscale/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from wagtail.core import blocks
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
@@ -11,17 +12,18 @@ from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.images.blocks import ImageChooserBlock
 
 
+class GrayscaleMastheadBlock(blocks.StructBlock):
+    heading = blocks.CharBlock()
+    subheading = blocks.RichTextBlock()
+    background_image = ImageChooserBlock()
+
+    class Meta:
+        template = 'grayscale/blocks/grayscale_masthead_block.html'
+
+
 class GrayscalePage(Page):
-    body = RichTextField(blank=True)
-
-    content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full"),
-    ]
-
-
-class GrayscaleMastheadPage(Page):
     body = StreamField([
-        ('heading', blocks.CharBlock(classname="full title")),
+        ('masthead', GrayscaleMastheadBlock()),
         ('paragraph', blocks.RichTextBlock()),
         ('image', ImageChooserBlock()),
     ])
