2f74985d088c32f9c4d477e8dfd7ab7b5db7ed6e
Marc LaBelle <marclabelle@pop-os.localdomain>
2018-12-23 09:00:10 -0500

    finished business casual except menus

--- bwagtail/bootstrap_business_casual/models.py
+++ bwagtail/bootstrap_business_casual/models.py
@@ -23,8 +23,8 @@ from bootstrap_common.models import *
 
 
 class BootstrapBusinessCasualMastheadBlock(blocks.StructBlock):
-    heading = blocks.CharBlock(required=False)
-    subheading = blocks.RichTextBlock(required=False)
+    heading = blocks.CharBlock()
+    subheading = blocks.RichTextBlock()
     background_image = ImageChooserBlock()
     masthead_image = ImageChooserBlock(help_text='Make sure this is a seamless image.')
     upper_text = blocks.TextBlock()
@@ -81,106 +81,16 @@ class BootstrapBusinessCasualAboutBlock(blocks.StructBlock):
 
 class BootstrapBusinessCasualProductsBlock(blocks.StructBlock):
     item = blocks.ListBlock(blocks.StructBlock([
-        ('heading_upper', blocks.CharBlock(required=False)),
-        ('heading_lower', blocks.CharBlock(required=False)),
-        ('paragraph', blocks.RichTextBlock(required=False)),
-        ('image', ImageChooserBlock(required=False)),
-        # ('link1', blocks.PageChooserBlock(required=False))
+        ('heading_upper', blocks.CharBlock()),
+        ('heading_lower', blocks.CharBlock()),
+        ('paragraph', blocks.TextBlock(help_text='Make sure you have over 80 characters to render properly.')),
+        ('image', ImageChooserBlock()),
         ]))
 
     class Meta:
         app_label = 'bootstrap_business_casual'
         template = 'bootstrap_business_casual/blocks/bootstrap_business_casual_products_block.html'
         label = 'Products'
-# 
-# class BootstrapBusinessCasualFeaturedLargeRowBlock(blocks.StructBlock):
-#     item = blocks.ListBlock(blocks.StructBlock([
-#         ('heading', blocks.CharBlock(required=False)),
-#         ('paragraph', blocks.RichTextBlock(required=False)),
-#         ('image', ImageChooserBlock()),
-#         ('image_position', blocks.ChoiceBlock(choices=(('left', 'Left'), ('right', 'Right'))))
-#     ]))
-# 
-#     class Meta:
-#         app_label = 'bootstrap_business_casual'
-#         template = 'bootstrap_business_casual/blocks/featured/business_casual_featured_large_row_block.html'
-#         label = 'Featured Large Row'
-# 
-# 
-# class BootstrapBusinessCasualFeaturedSmallRowBlock(blocks.StructBlock):
-#     item = blocks.ListBlock(blocks.StructBlock([
-#         ('heading', blocks.CharBlock(required=False)),
-#         ('paragraph', blocks.RichTextBlock(required=False)),
-#         ('image', ImageChooserBlock()),
-#         ('image_position', blocks.ChoiceBlock(choices=(('left', 'Left'), ('right', 'Right'))))
-#         ]))
-# 
-#     class Meta:
-#         app_label = 'bootstrap_business_casual'
-#         template = 'bootstrap_business_casual/blocks/featured/business_casual_featured_small_row_block.html'
-#         label = 'Featured Small Row'
-# 
-# 
-# class BootstrapBusinessCasualFeaturedBlock(blocks.StructBlock):
-#     featured = blocks.StreamBlock([
-#         ('featured_row_large', BootstrapBusinessCasualFeaturedLargeRowBlock()),
-#         ('featured_row', BootstrapBusinessCasualFeaturedSmallRowBlock()),
-#     ])
-# 
-#     class Meta:
-#         app_label = 'bootstrap_business_casual'
-#         template = 'bootstrap_business_casual/blocks/featured/business_casual_featured_block.html'
-#         label = 'Featured'
-# 
-# 
-# class BootstrapBusinessCasualSignupBlock(blocks.StructBlock):
-#     heading = blocks.CharBlock(required=False)
-#     paragraph = blocks.RichTextBlock(required=False)
-#     background_image = ImageChooserBlock()
-# 
-#     class Meta:
-#         app_label = 'bootstrap_business_casual'
-#         template = 'bootstrap_business_casual/blocks/business_casual_signup_block.html'
-# 
-# 
-# class BootstrapBusinessCasualContactBlock(blocks.StructBlock):
-#     title = blocks.CharBlock(required=False)
-#     paragraph = blocks.RichTextBlock(required=False)
-#     address = blocks.TextBlock()
-#     email = blocks.CharBlock()
-#     phone = blocks.CharBlock()
-#     SOCIAL_MEDIA = (
-#         ('fab fa-facebook-f', 'Facebook'),
-#         ('fa-instagram', 'Instagram'),
-#         ('fab fa-linkedin', 'LinkedIn'),
-#         ('fab fa-twitter', 'Twitter'),
-#         ('fab fa-pinterest', 'Pinterest'),
-#         ('fab fa-youtube', 'Youtube'),
-#         ('fab fa-github', 'GitHub'),
-#         ('fab fa-gitlab', 'GitLab'),
-#     )
-#     social_media = blocks.ListBlock(blocks.StructBlock([
-#         ('social_media', blocks.ChoiceBlock(SOCIAL_MEDIA)),
-#         ('social_link', blocks.URLBlock()),
-#     ], required=False))
-# 
-#     def get_context(self, value, parent_context=None):
-#         context = super().get_context(value, parent_context=parent_context)
-#         address_query = value['address']
-# 
-#         # Remove all non-word characters (everything except numbers and letters)
-#         import re
-#         address_query = re.sub(r"[^\w\s]", '', address_query)
-# 
-#         # Replace all runs of whitespace with a single dash
-#         address_query = re.sub(r"\s+", '+', address_query)
-# 
-#         context['value']['address_query'] = address_query
-#         return context
-# 
-#     class Meta:
-#         app_label = 'bootstrap_business_casual'
-#         template = 'bootstrap_business_casual/blocks/business_casual_contact_block.html'
 
 
 class BootstrapBusinessCasualPage(Page):
